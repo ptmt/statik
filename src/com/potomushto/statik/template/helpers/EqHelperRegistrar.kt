7562de3fb93573199959ca35879524fa22935c5b
Dmitry Loktev <dmitry.loktev@jetbrains.com>
2025-11-05 20:49:00 +0100

    Fix eq

--- src/com/potomushto/statik/template/helpers/EqHelperRegistrar.kt
+++ src/com/potomushto/statik/template/helpers/EqHelperRegistrar.kt
@@ -2,12 +2,30 @@ package com.potomushto.statik.template.helpers
 
 import com.github.jknack.handlebars.Handlebars
 import com.github.jknack.handlebars.Helper
+import com.github.jknack.handlebars.Options
 
 class EqHelperRegistrar : HandlebarsHelperRegistrar {
     override fun register(handlebars: Handlebars, context: HelperRegistrationContext) {
         handlebars.registerHelper("eq", Helper<Any> { value, options ->
             val param = options?.param<Any>(0)
-            value == param
+            val isEqual = value == param
+
+            // Check tagType to determine if this is a block helper or subexpression
+            // SECTION = block helper like {{#eq}}...{{/eq}}
+            // VAR = inline/subexpression like {{eq}} or {{#if (eq ...)}}
+            val isBlockHelper = options.tagType.toString() == "SECTION"
+
+            if (isBlockHelper) {
+                // Block helper mode: {{#eq value param}}content{{else}}alt{{/eq}}
+                if (isEqual) {
+                    options.fn()
+                } else {
+                    options.inverse()
+                }
+            } else {
+                // Inline/subexpression mode: {{#if (eq value param)}}
+                isEqual
+            }
         })
     }
 }
